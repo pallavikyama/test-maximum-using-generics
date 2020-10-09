a7e5a62b68fdd870476844a1fb09143b4a868247
pallavikyama <kyamap98@gmail.com>
2020-10-09 14:54:19 +0530

    Modified:Generic printMax method is called from findMax method

--- MaxOfThreeValues.java
+++ MaxOfThreeValues.java
@@ -1,19 +1,21 @@
 package com.blz.testmaximum;
 
 public class MaxOfThreeValues<E extends Comparable<E>> {
-
 	protected static <E extends Comparable<E>> E findMax(E x, E... values) {
-		System.out.println("The greatest of the three values is: ");
 		E max = x;
 		for (E i : values) {
 			if (i.compareTo(max) > 0)
 				max = i;
 		}
-		System.out.println(max);
+		printMax(max);
 		return max;
 	}
 
+	protected static<E extends Comparable<E>> void printMax(E max) {
+		System.out.println("The greatest of the three values is: " + max);
+	}
+
 	public static void main(String[] args) {
-		findMax(315, 325, 300);
+		findMax(315,325,300);
 	}
 }
