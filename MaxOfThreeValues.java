a35a9be5f146c24e0ad26b9dba11574a3c83db3d
pallavikyama <kyamap98@gmail.com>
2020-10-09 14:10:00 +0530

    Refactor:Multiple inputs are given as arguments to find maximum value

--- MaxOfThreeValues.java
+++ MaxOfThreeValues.java
@@ -2,39 +2,18 @@ package com.blz.testmaximum;
 
 public class MaxOfThreeValues<E extends Comparable<E>> {
 
-	E x, y, z; // VARIABLES
-
-	protected MaxOfThreeValues(E x, E y, E z) { // PARAMETER CONSTRUCTOR
-		this.x = x;
-		this.y = y;
-		this.z = z;
-		findMax(x, y, z);
-	}
-
-	protected static <E extends Comparable<E>> E findMax(E x, E y, E z) {
+	protected static <E extends Comparable<E>> E findMax(E x, E... values) {
 		System.out.println("The greatest of the three values is: ");
-		if (x.compareTo(y) > 0) {
-			if (y.compareTo(z) > 0 || x.compareTo(z) > 0) {
-				System.out.println(x);
-				return x;
-			}
-			else {
-				System.out.println(z);
-				return z;
-			}
-		} else {
-			if (y.compareTo(z) > 0) {
-				System.out.println(y);
-				return y;
-			}
-			else {
-				System.out.println(z);
-				return z;
-			}
+		E max = x;
+		for (E i : values) {
+			if (i.compareTo(max) > 0)
+				max = i;
 		}
+		System.out.println(max);
+		return max;
 	}
 
 	public static void main(String[] args) {
-		new MaxOfThreeValues<Integer>(315, 325, 300);
+		findMax(315, 325, 300);
 	}
 }
--- TestCaseForMaxValue.java
+++ TestCaseForMaxValue.java
@@ -8,7 +8,7 @@ public class TestCaseForMaxValue {
 
 	// TEST CASES - 1.1, 1.2, 1.3
 	@Test
-	public void TestMaxInt() {
+	public void testMaxInt() {
 		Integer expected = 3;
 		Integer actual1 = MaxOfThreeValues.findMax(3, 2, 1); // MAX INTEGER AT POSITION-1
 		assertEquals(expected, actual1);
@@ -20,7 +20,7 @@ public class TestCaseForMaxValue {
 
 	// TEST CASES - 2.1, 2.2, 2.3
 	@Test
-	public void TestMaxFloat() {
+	public void testMaxFloat() {
 		Float expected = 3f;
 		Float actual1 = MaxOfThreeValues.findMax(3f, 2f, 1f); // MAX FLOAT NUMBER AT POSITION-1
 		assertEquals(expected, actual1);
@@ -32,7 +32,7 @@ public class TestCaseForMaxValue {
 
 	// TEST CASES - 3.1, 3.1, 3.3
 	@Test
-	public void TestMaxString() {
+	public void testMaxString() {
 		String expected = "Peach";
 		String actual1 = MaxOfThreeValues.findMax("Peach", "Apple", "Banana"); // MAX STRING AT POSITION-1
 		assertEquals(expected, actual1);
@@ -40,5 +40,21 @@ public class TestCaseForMaxValue {
 		assertEquals(expected, actual2);
 		String actual3 = MaxOfThreeValues.findMax("Apple", "Banana", "Peach"); // MAX STRING AT POSITION-3
 		assertEquals(expected, actual3);
+	}
+
+	@Test
+	public void variableArguments() {
+		String expected1 = "Canvas";
+		String actual1 = MaxOfThreeValues.findMax("Can", "Canvas");
+		assertEquals(expected1, actual1);
+		String actual2 = MaxOfThreeValues.findMax("Canvas", "Candle", "Can");
+		assertEquals(expected1, actual2);
+		String actual3 = MaxOfThreeValues.findMax("Candle", "Can", "Candy", "Canvas");
+		assertEquals(expected1, actual3);
+		Float expected2=1024f;
+		Float actual4 = MaxOfThreeValues.findMax(1024f,256f,128f);
+		assertEquals(expected2, actual4);
+		Float actual5 = MaxOfThreeValues.findMax(8f,256f,128f,1024f);
+		assertEquals(expected2, actual5);
 	}
 }
