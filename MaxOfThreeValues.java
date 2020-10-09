5c7ed507c5ff3ce906c5ce4decf04bdf358541a6
pallavikyama <kyamap98@gmail.com>
2020-10-09 10:21:55 +0530

    Refactor-1:Method made generic to find max integer or float-value or string

--- MaxOfThreeValues.java
+++ MaxOfThreeValues.java
@@ -2,7 +2,7 @@ package com.blz.testmaximum;
 
 public class MaxOfThreeValues {
 
-	protected static String findMaxString(String x, String y, String z) {
+	protected static <E extends Comparable<E>> E findMax(E x, E y, E z) {
 		if (x.compareTo(y) > 0) {
 			if (y.compareTo(z) > 0 || x.compareTo(z) > 0)
 				return x;
@@ -17,6 +17,8 @@ public class MaxOfThreeValues {
 	}
 
 	public static void main(String[] args) {
-		System.out.println("The greatest of the three integers is: " + findMaxString("cat", "rat", "mat"));
+		System.out.println("The greatest of the three integers is: " + findMax(315, 325, 300));
+		System.out.println("The greatest of the three float numbers is: " + findMax(315f, 325f, 300f));
+		System.out.println("The greatest of the three strings is: " + findMax("cat", "rat", "mat"));
 	}
 }
--- TestCaseForMaxValue.java
+++ TestCaseForMaxValue.java
@@ -6,15 +6,39 @@ import org.junit.Test;
 
 public class TestCaseForMaxValue {
 
-	// TEST CASES-1.1,1.2,1.3
+	// TEST CASES - 1.1, 1.2, 1.3
+	@Test
+	public void TestMaxInt() {
+		Integer expected = 3;
+		Integer actual1 = MaxOfThreeValues.findMax(3, 2, 1); // MAX INTEGER AT POSITION-1
+		assertEquals(expected, actual1);
+		Integer actual2 = MaxOfThreeValues.findMax(2, 3, 1); // MAX INTEGER AT POSITION-2
+		assertEquals(expected, actual2);
+		Integer actual3 = MaxOfThreeValues.findMax(1, 2, 3); // MAX INTEGER AT POSITION-3
+		assertEquals(expected, actual3);
+	}
+
+	// TEST CASES - 2.1, 2.2, 2.3
 	@Test
 	public void TestMaxFloat() {
-		String expected = "Peach";
-		String actual1 = MaxOfThreeValues.findMaxString("Peach", "Apple", "Banana"); // MAX AT POSITION-1
+		Float expected = 3f;
+		Float actual1 = MaxOfThreeValues.findMax(3f, 2f, 1f); // MAX FLOAT NUMBER AT POSITION-1
 		assertEquals(expected, actual1);
-		String actual2 = MaxOfThreeValues.findMaxString("Apple", "Peach", "Banana"); // MAX AT POSITION-2
+		Float actual2 = MaxOfThreeValues.findMax(2f, 3f, 1f); // MAX FLOAT NUMBER AT POSITION-2
 		assertEquals(expected, actual2);
-		String actual3 = MaxOfThreeValues.findMaxString("Apple", "Banana", "Peach"); // MAX AT POSITION-3
+		Float actual3 = MaxOfThreeValues.findMax(1f, 2f, 3f); // MAX FLOAT NUMBER AT POSITION-3
+		assertEquals(expected, actual3);
+	}
+
+	// TEST CASES - 3.1, 3.1, 3.3
+	@Test
+	public void TestMaxString() {
+		String expected = "Peach";
+		String actual1 = MaxOfThreeValues.findMax("Peach", "Apple", "Banana"); // MAX STRING AT POSITION-1
+		assertEquals(expected, actual1);
+		String actual2 = MaxOfThreeValues.findMax("Apple", "Peach", "Banana"); // MAX STRING AT POSITION-2
+		assertEquals(expected, actual2);
+		String actual3 = MaxOfThreeValues.findMax("Apple", "Banana", "Peach"); // MAX STRING AT POSITION-3
 		assertEquals(expected, actual3);
 	}
 }
